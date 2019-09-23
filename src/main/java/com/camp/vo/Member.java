f6e145c9b7d5a40a761b229d5e0fa9961702cf2b
joyechan <joyechan@DESKTOP-LLE6PP0>
2019-09-23 17:54:51 +0900

    19.09.23 Member Vo	-예찬

---
+++ src/main/java/com/camp/vo/Member.java
@@ -0,0 +1,21 @@
+package com.camp.vo;
+
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class Member {
+	
+	private String memberId;
+	private String pwd;
+	private String name;
+	private String phone;
+	private String type;	
+	private String email;	
+	private String addr1;
+	private String addr2;
+	private String addr3;
+	private Date regDate;
+}
