dfe0b40db07be50cd91445e25c5e5bc4607e903c
goott6-9 <goott6-9@goott6-9-PC>
2019-09-26 10:06:46 +0900

    vo

---
+++ src/main/java/com/camp/vo/Camp.java
@@ -0,0 +1,15 @@
+package com.camp.vo;
+
+
+import lombok.Data;
+
+@Data
+public class Camp {
+	
+	private int campNo;	
+	private String campName;	
+	private String content;
+	private String price;
+	private String category;
+	private String memberId;
+}
